4ee587dc723672a0c95c370628902868d636858e
chunting <chunting0618@gmail.com>
2026-05-28 13:43:12 +0800

    fix(home-activities): normalize activity API response

--- src/modules/activity/api/activityApi.js
+++ src/modules/activity/api/activityApi.js
@@ -1,5 +1,6 @@
 import { httpClient } from '@/shared/api/httpClient';
 
 export async function getActivities(params = {}) {
-  return httpClient.get('/activities', { params });
+  const response = await httpClient.get('/activities', { params });
+  return response.data;
 }
