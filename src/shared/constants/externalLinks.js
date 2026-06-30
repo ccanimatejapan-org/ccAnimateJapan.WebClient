9b45584eb6f998846e9b49cf6be023e3ffd8e763
tim0618 <tim061801@gmail.com>
2026-07-01 00:45:38 +0800

    chore: 新增官方連結

--- src/shared/constants/externalLinks.js
+++ src/shared/constants/externalLinks.js
@@ -1,7 +1,7 @@
 // 對外連結集中管理。請把 '#' 換成正式網址（LINE、Instagram、Threads 等）。
 export const externalLinks = {
-  lineOfficial: '#', // LINE 官方帳號（加好友／購物）
-  lineCommunity: '#', // LINE 社群
-  instagram: '#', // Instagram 帳號
-  threads: '#' // Threads 帳號
+  lineOfficial: 'https://line.me/R/ti/p/@680javyx', // LINE 官方帳號（加好友／購物）
+  lineCommunity: 'https://line.me/ti/g2/TsrTLSYyE1Bqkt8-FrxqnwSSijQtbwXUtgLRuQ?utm_source=invitation&utm_medium=link_copy&utm_campaign=default', // LINE 社群
+  instagram: 'https://www.instagram.com/animate.store.11?igsh=dTR0anY2NnBvYjNz&utm_source=qr', // Instagram 帳號
+  threads: 'https://www.threads.com/@cc.animate.store?igshid=NTc4MTIwNjQ2YQ==' // Threads 帳號
 };
